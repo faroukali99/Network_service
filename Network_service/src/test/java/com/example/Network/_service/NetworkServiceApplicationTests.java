21dfccdbfa95c07a97837284b42a01009ba74d97
faroukali99 <[faroukoualiou2005@gmail.com]>
2026-01-02 01:13:47 +0100

    Initial commit

---
+++ Network_service/src/test/java/com/example/Network/_service/NetworkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Network._service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
